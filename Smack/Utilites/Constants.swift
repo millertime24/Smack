2ab0ac0d03afbc33e940704ccd7407f75c6f10e8
Andrew <milldog8@hotmail.com>
2017-09-08 13:58:03 -0600

    new login vc and segues

---
+++ Smack/Utilites/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Andrew on 9/8/17.
+//  Copyright © 2017 Andrew. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+
+let TO_LOGIN = "toLogin"
