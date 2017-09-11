8368753fb0d0c861e134416d5704e488ce9cbc1a
Andrew <milldog8@hotmail.com>
2017-09-11 16:44:12 -0600

    Auth Service and Constants

--- Smack/Utilites/Constants.swift
+++ Smack/Utilites/Constants.swift
@@ -8,8 +8,19 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Sucess: Bool) -> ()
+
+// URL
+let BASE_URL = "https://chattychat2.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
 //Segues
 
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+// User Defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
+
