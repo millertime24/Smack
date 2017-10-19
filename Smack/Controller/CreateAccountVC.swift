d6dd8d9ae999c7bb76e40406d07b0ec8d6648d20
Andrew <milldog8@hotmail.com>
2017-10-19 14:50:36 -0600

    create user

--- Smack/Controller/CreateAccountVC.swift
+++ Smack/Controller/CreateAccountVC.swift
@@ -16,6 +16,10 @@ class CreateAccountVC: UIViewController {
     
     @IBOutlet weak var userImg: UIImageView!
     
+    //variables
+    var avatarName = "profileDefault"
+    var avatarColor = "[0.5, 0.5, 0.5, 1]"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -31,6 +35,8 @@ class CreateAccountVC: UIViewController {
     }
     
     @IBAction func createAccountPressed(_ sender: Any) {
+        guard let name = usernameTxt.text , usernameTxt.text != "" else
+        {return}
         guard let email = emailTxt.text , emailTxt.text != "" else
         {return}
     guard let pass = passTxt.text , passTxt.text != "" else
@@ -39,7 +45,16 @@ class CreateAccountVC: UIViewController {
             if success {
                 AuthService.instance.loginUser(email: email, password: pass, completion: { (success) in
                     if success {
-                        print("logged in user!", AuthService.instance.authToken)
+                        AuthService.instance.createUser(name: name, email: email, avatarName: self.avatarName, avatarColor: self.avatarColor, completion: { (success) in
+                            if success {
+                                print(UserDataService.instance.name,
+                                      UserDataService.instance.avatarName)
+                                self.performSegue(withIdentifier: UNWIND, sender: nil)
+                               
+                            }
+                            
+                            
+                        })
                     }
             })
         }
