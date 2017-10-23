2217225f42df6f888aedba83cacdc6c8ceadfe55
Andrew <milldog8@hotmail.com>
2017-10-23 12:13:10 -0600

    channel tableview

---
+++ Smack/View/ChannelCell.swift
@@ -0,0 +1,40 @@
+//
+//  ChannelCell.swift
+//  Smack
+//
+//  Created by Andrew on 10/23/17.
+//  Copyright © 2017 Andrew. All rights reserved.
+//
+
+import UIKit
+
+class ChannelCell: UITableViewCell {
+    
+    //outlets
+    @IBOutlet weak var channelName: UILabel!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        if selected {
+            self.layer.backgroundColor = UIColor(white:1, alpha: 0.2).cgColor
+        }else {
+            self.layer.backgroundColor = UIColor.clear.cgColor
+        }
+        
+    }
+
+    func configureCell(channel: Channel) {
+        let title = channel.channelTitle ?? ""
+        channelName.text = "#\(title)"
+        
+    }
+    
+    
+}
